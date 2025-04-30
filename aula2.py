03ef49c4380ed9468966ac9996eeee628a2331f2
ahvShiro <arthurshiro123@gmail.com>
2025-04-30 12:41:19 -0300

    Aula 2 refeita com pandas

--- aula2.py
+++ aula2.py
@@ -1,24 +1,28 @@
-import numpy as np
+import panda as pd
 
-dados = np.genfromtxt("pedrinhas.csv", delimiter=",", skip_header=1)
+df = pd.read_csv('pedrinhas.csv')
+
+lista_desceu = list(df['desceu'])
+lista_distancia = list(df['distancia'])
+lista_classe = list(df['classificacao'])
 
 joao = 0
 maior_distancia_joao = 0.0
 maria = 0
 maior_desceu_maria = 0.0
 
-for linha in dados:
-    if linha[2] == 1:
+for desceu, distancia, classe in zip(lista_desceu, lista_distancia, lista_classe):
+    if classe == 1:
         joao += 1
-        if linha[0] > maior_distancia_joao:
-            maior_distancia_joao = linha[0]
-
-    elif linha[2] == 2:
+        if distancia > maior_distancia_joao:
+            maior_distancia_joao = distancia
+    elif classe == 2:
         maria += 1
-        if linha[1] > maior_desceu_maria:
-            maior_desceu_maria = linha[1]
+        if desceu > maior_desceu_maria:
+            maior_desceu_maria = desceu
 
 print("joao: ", joao)
 print("maria: ", maria)
 print("maior_distancia_joao: ", maior_distancia_joao)
 print("maior_desceu_maria: ", maior_desceu_maria)
+
